4576f611e608b1b15186d6dd76ae54ad74f23f16
PHYO-AUNG-NAING-TUN <phyotun24@gmail.com>
2026-08-11 23:24:48 +0630

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  nitro: {
-    preset: 'vercel',
-  },
-
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
